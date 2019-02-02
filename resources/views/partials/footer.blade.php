394b568666f329d10d8d4aefe78e9e3888bae57d
huras <hurast@gmail.com>
2019-02-02 12:39:19 -0200

    Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,23 +1,35 @@
 <div class='container-fluid'>
     <div class='row'>
-        <div class='footer' style='width: 100%;'>
-            <div class='col-md-12 social-nets'>
-                <a class='social-net-icon' title='faceboook'>
+        <div class='footer' style='width: 100%; display: flex; flex-wrap: wrap;'>
+            <div class='col-md-4 infos'>
+                <div class='titulo'> Sogniamo in Grande ® </div>
+                <div class='sub-titulo'> All rights reserved 2019 </div>
+            </div>
+
+            <div class='col-md-4 social-nets'>
+                <a class='social-net-icon' title='faceboook' target='_blank' href='https://www.facebook.com/Sogniamo-in-Grande-278875232794723/'>
                     <i class="fab fa-facebook"></i>
                 </a>
-                <a class='social-net-icon' title='linkedin'>
+                <a class='social-net-icon' title='linkedin' target='_blank' href='https://www.linkedin.com/in/sogniamo-in-grande-marco-polito-a94748a7/'>
                     <i class="fab fa-linkedin-in"></i>
                 </a>
-                <a class='social-net-icon' title='instagram'>
+                <a class='social-net-icon' title='instagram' target='_blank' href='https://www.instagram.com/sogniamoingrande/'>
                     <i class="fab fa-instagram"></i>
                 </a>
-                <a class='social-net-icon' title='youtube'>
+                <a class='social-net-icon' title='youtube' target='_blank' href='https://www.youtube.com/channel/UCL6PL_3UhIO-VqFJ_IJWu3g'>
                     <i class="fab fa-youtube"></i>
                 </a>
             </div>
-            <div class='col-md-12 infos'>
-                <div class='titulo'> Sogniamo in Grande® </div>
-                <div class='sub-titulo'> All rights reserved 2019 </div>
+
+            <div class='col-md-4 contacts'>
+                <div class='contact-info'> 
+                    <i class="far fa-envelope"></i> 
+                    <div class='value'> sogniamoingrande@yahoo.com </div>
+                </div>
+                <div class='contact-info'> 
+                    <i class="fab fa-whatsapp"></i> 
+                    <div class='value'> +39 328 973 3662 </div>
+                </div>
             </div>
         </div>
     </div>
