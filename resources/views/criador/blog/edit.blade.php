ffa35f68f9d017c2023e2e8981c431df1b338d65
huras <hurast@gmail.com>
2019-02-03 16:09:44 -0200

    Blog Edit Layout

--- resources/views/criador/blog/edit.blade.php
+++ resources/views/criador/blog/edit.blade.php
@@ -13,89 +13,110 @@
 
         <div class='row criador-form'>
             <form class='w-100' method="POST" action="/admin/blog/{{$post->id}}/update" enctype="multipart/form-data" style='padding-top: 24px; padding-bottom: 64px; '>
-                {{ csrf_field() }}
-
-                <div class='form-group col-md-12'>
-                    <label> {{ __('Published') }} </label>
-                    <input type='checkbox' name='status' <?php if($post->status == 1) echo " checked "; ?>>
-                    @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'status'])
-                </div>
-
-                <div class='form-group col-md-12'>
-                    <div class='separator'></div>
-                </div>
-                <!-- ==================================================================== -->
-                <div class='form-group col-md-12'>
-                    <label class='area'> {{ __('Cover') }} </label>
-                </div>
-                
-                <div class='form-group col-md-12'>
-                    <select id='cover_type_select' class="{{ $errors->has('cover_type_id') ? ' is-invalid' : '' }}" name='cover_type_id' onchange='onChangeCoverType()'>
-                        @foreach($cover_types as $type)
-                            <option <?php if($post->cover_type_id == $type->id) echo " selected "; ?> value='{{$type->id}}' > {{$type->name}} </option>
-                        @endforeach
-                    </select>
-                    @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'cover_type_id'])
-                </div>
-
-                <div class='form-group col-md-12' id='image-input'>
-                    <input class="{{ $errors->has('cover') ? ' is-invalid' : '' }}" type='file' accept="image/*" name='cover' value="<?php if($post->cover_type_id == 1) echo $post->cover; ?>" >
-                    @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'cover'])
-                    @if($post->cover_type_id == 1)
-                        <div class='current' style='margin-top: 16px;'>
-                            <img src="{{asset('images/posts/'.$post->id.'/'.$post->cover)}}">
-                        </div>
-                    @endif
-                </div>
-
-                <div class='form-group w-100' id='video-input' style='display: flex; flex-direction: row; align-items: center; flex-wrap: wrap;'>
-                    <div class='col-md-1'>
-                        <label style='width:unset;'> {{ __('Link') }} </label>
-                    </div>
-                    <div class='col-md-11'>
-                        <input class="{{ $errors->has('cover') ? ' is-invalid' : '' }}" type='text' name='cover' value="<?php if($post->cover_type_id == 2) echo 'https://www.youtube.com/watch?v='.$post->cover; ?>" >
-                    </div>
-                    @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'cover'])
-                    @if($post->cover_type_id == 2)
-                        <div class='current video col-md-12' style='margin-top: 16px;'>
-                            <iframe width="100%" height="550" src="https://www.youtube.com/embed/{{$post->cover}}"> </iframe>
-                        </div>
-                    @endif
-                </div>
-                <!-- ==================================================================== -->
-                <div class='form-group col-md-12'>
-                    <div class='separator'></div>
-                </div>
-
-                <div class='form-group col-md-12'>
-                    <label class='area'> {{ __('Contents') }} </label>
-                </div>
-
-                <div class='form-group col-md-6'>
-                    <label> {{ __('Title') }} </label>
-                    <input class="{{ $errors->has('title') ? ' is-invalid' : '' }}" type='text' name='title' value="<?php if(old('title')) echo old('title'); else echo $post->title; ?>" required autofocus>
-                    @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'title'])
-                </div>
-
-                <input type="hidden" name="slug" value="123">
-
-                <div class='form-group col-md-6'>
-                    <label> {{ __('Call') }} </label>
-                    <input class="{{ $errors->has('call') ? ' is-invalid' : '' }}" type='text' name='call' value="<?php if(old('call')) echo old('call'); else echo $post->call; ?>">
-                    @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'call'])
-                </div>
-
-                <div class='form-group col-md-12'>
-                    <label> {{ __('Content') }} </label>
-                    <textarea class="{{ $errors->has('content') ? ' is-invalid' : '' }}" id='blog-ckeditor' name='content'><?php if(old('content')) echo old('content'); else echo $post->content; ?></textarea>
-                    @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'content'])
-                </div>
-
-                <div class='form-group col-md-6'>
+                <div class='form-group w-100'>
                     <input type='submit' class='my-btn save' value='Save'>
                 </div>
-                <div class='form-group col-md-6'>
-                    <a href='/admin/blog' class='my-btn back' > Back </a>
+                {{ csrf_field() }}
+
+                <div class="bd-example bd-example-tabs w-100">
+                    <ul class="nav nav-tabs w-100" id="myTab" role="tablist">
+                        <div class='form-group col-md-12'>
+                            <div class='separator'></div>
+                        </div>
+                        <li class="nav-item">
+                        <a class="nav-link" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="false">Post Status</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Cover</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active show" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="true">Content</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content w-100" id="myTabContent">
+                        <div class="tab-pane fade row" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            <div class='form-group col-md-12'>
+                                <div class='separator'></div>
+                            </div>
+                            <div class='form-group col-md-12'>
+                                <label> {{ __('Published') }} </label>
+                                <input type='checkbox' name='status' <?php if($post->status == 1) echo " checked "; ?>>
+                                @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'status'])
+                            </div>
+                        </div>
+                        <div class="tab-pane fade row" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <div class='form-group col-md-12'>
+                                <div class='separator'></div>
+                            </div>
+                            
+                            <div class='form-group col-md-12'>
+                                <select id='cover_type_select' class="{{ $errors->has('cover_type_id') ? ' is-invalid' : '' }}" name='cover_type_id' onchange='onChangeCoverType()'>
+                                    <option value='0'> None </option>
+                                    @foreach($cover_types as $type)
+                                        <option <?php if($post->cover_type_id == $type->id) echo " selected "; ?> value='{{$type->id}}' > {{$type->name}} </option>
+                                    @endforeach
+                                </select>
+                                @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'cover_type_id'])
+                            </div>
+
+                            <div class='form-group col-md-12' id='image-input'>
+                                <input class="{{ $errors->has('cover') ? ' is-invalid' : '' }}" type='file' accept="image/*" name='cover' value="<?php if($post->cover_type_id == 1) echo $post->cover; ?>" >
+                                @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'cover'])
+                                @if($post->cover_type_id == 1)
+                                    <div class='current' style='margin-top: 16px;'>
+                                        <img src="{{asset('images/posts/'.$post->id.'/'.$post->cover)}}">
+                                    </div>
+                                @endif
+                            </div>
+
+                            <div class='form-group w-100' id='video-input' style='display: flex; flex-direction: row; align-items: center; flex-wrap: wrap;'>
+                                <div class='col-md-1'>
+                                    <label style='width:unset;'> {{ __('Link') }} </label>
+                                </div>
+                                <div class='col-md-11'>
+                                    <input class="{{ $errors->has('cover') ? ' is-invalid' : '' }}" type='text' name='cover' value="<?php if($post->cover_type_id == 2) echo 'https://www.youtube.com/watch?v='.$post->cover; ?>" >
+                                </div>
+                                @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'cover'])
+                                @if($post->cover_type_id == 2)
+                                    <div class='current video col-md-12' style='margin-top: 16px;'>
+                                        <iframe width="100%" height="550" src="https://www.youtube.com/embed/{{$post->cover}}"> </iframe>
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="tab-pane fade row active show" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                            <div class='form-group col-md-12'>
+                                <div class='separator'></div>
+                            </div>
+
+                            <div class='form-group col-md-6'>
+                                <label> {{ __('Title') }} </label>
+                                <input class="{{ $errors->has('title') ? ' is-invalid' : '' }}" type='text' name='title' value="<?php if(old('title')) echo old('title'); else echo $post->title; ?>" required autofocus>
+                                @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'title'])
+                            </div>
+
+                            <input type="hidden" name="slug" value="123">
+
+                            <div class='form-group col-md-6'>
+                                <label> {{ __('Call') }} </label>
+                                <input class="{{ $errors->has('call') ? ' is-invalid' : '' }}" type='text' name='call' value="<?php if(old('call')) echo old('call'); else echo $post->call; ?>">
+                                @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'call'])
+                            </div>
+
+                            <div class='form-group col-md-12'>
+                                <label> {{ __('Content') }} </label>
+                                <textarea class="{{ $errors->has('content') ? ' is-invalid' : '' }}" id='blog-ckeditor' name='content'><?php if(old('content')) echo old('content'); else echo $post->content; ?></textarea>
+                                @include('partials/form-errors', ['errors' => $errors, 'fieldName' => 'content'])
+                            </div>
+
+                            <div class='form-group col-md-6'>
+                                <input type='submit' class='my-btn save' value='Save'>
+                            </div>
+                            <div class='form-group col-md-6'>
+                                <a href='/admin/blog' class='my-btn back' > Back </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </form>
         </div>
@@ -113,9 +134,13 @@
                 imageInput.style.display = "flex";
                 videoInput.style.display = "none";
             }
-            else {
+            else if(coverTypeSelect.value == 2) {
                 imageInput.style.display = "none";
                 videoInput.style.display = "flex";
+            }
+            else{
+                imageInput.style.display = "none";
+                videoInput.style.display = "none";
             }
         }
 
