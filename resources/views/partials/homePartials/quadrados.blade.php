d72f1a3c958c661a165e81663bd41ddd988f521d
huras <hurast@gmail.com>
2019-02-03 15:55:23 -0200

    Mosaics Update

--- resources/views/partials/homePartials/quadrados.blade.php
+++ resources/views/partials/homePartials/quadrados.blade.php
@@ -2,31 +2,30 @@
     <div class='mosaics row'>
             <?php 
                 $topMosaics = [
-                    ['title' => 'Lorem ipsum dolor sit amet', 'call' => 'Hoje o ceu estava azul.', 'cover' => '1.jpg'],
-                    ['title' => 'Uso roupa de frio no inverno todo dia.', 'call' => 'Quem gosta de pudim é tipo eu.', 'cover' => '2.jpg']
+                    ['title' => 'Facciamo un C.V. vincente', 'call' => '', 'cover' => 'q5tPsoSwrq4', 'url' => '', 'cover-type' => 2]                    
                 ];
 
                 $bottomMosaics = [
-                    ['title' => 'Lorem ipsum dolor sit amet', 'call' => 'Hoje o ceu estava azul.', 'cover' => '3.jpg'],
-                    ['title' => 'Uso roupa de frio no inverno todo dia.', 'call' => 'Quem gosta de pudim é tipo eu.', 'cover' => '5.jpg'],
-                    ['title' => 'Uso roupa de frio no inverno todo dia.', 'call' => 'Quem gosta de pudim é tipo eu.', 'cover' => '8.jpg']
+                    ['title' => 'Il saturatore', 'call' => '', 'cover' => '2.jpg', 'url' => 'https://www.ilsaturatore.it', 'cover-type' => 1],
+                    ['title' => 'Uso roupa de frio no inverno todo dia.', 'call' => 'Quem gosta de pudim é tipo eu.', 'cover' => '5.jpg', 'url' => '/blog', 'cover-type' => 1],
+                    ['title' => 'ASSIST TO THE WEBINAR.', 'call' => '', 'cover' => 'webinar.jpg', 'url' => '', 'cover-type' => 1]
                 ];
             ?>
 
             @foreach($topMosaics as $mosaic)
                 <?php $imageAddress = "img/dev/".$mosaic['cover']; ?>
-                <div class='mosaic top col-md-6'>
+                <a target='_blank' href="{{$mosaic['url']}}" class='mosaic top col-md-6 nlink'>
                     @include('partials/mosaic', $mosaic)
-                </div>
+                </a>
             @endforeach
     </div>
     
     <div class='mosaics row'>
         @foreach($bottomMosaics as $mosaic)
             <?php $imageAddress = "img/dev/".$mosaic['cover']; ?>
-            <div class='mosaic bottom col-md-4'>
+            <a target='_blank' href="{{$mosaic['url']}}" class='mosaic bottom col-md-4 nlink'>
                 @include('partials/mosaic', $mosaic)
-            </div>
+            </a>
         @endforeach
     </div>
 </div>
--- resources/views/partials/mosaic.blade.php
+++ resources/views/partials/mosaic.blade.php
@@ -1,4 +1,12 @@
-<div class='cover'  style="background-image: url('{{asset($imageAddress)}}');">
-    <div class='title'> {{$mosaic['title']}} </div>
-    <div class='call'> {{$mosaic['call']}} </div>
-</div>
+@if($mosaic['cover-type'] == 1)
+    <div class='cover'  style="background-image: url('{{asset($imageAddress)}}');">
+        <div class='title'> {{$mosaic['title']}} </div>
+        <div class='call'> {{$mosaic['call']}} </div>
+    </div>
+@elseif($mosaic['cover-type'] == 2)
+    <div class='cover'>
+        <iframe width="100%" height="400" src="https://www.youtube.com/embed/{{asset($imageAddress)}}"></iframe>
+        <div class='title'> {{$mosaic['title']}} </div>
+        <div class='call'> {{$mosaic['call']}} </div>
+    </div>
+@endif
