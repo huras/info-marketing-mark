09edad9fb3f0604f48cc129223c0839dcb672182
huras <hurast@gmail.com>
2019-02-05 22:24:32 -0200

    Mosaic update

--- resources/views/partials/homePartials/quadrados.blade.php
+++ resources/views/partials/homePartials/quadrados.blade.php
@@ -3,12 +3,13 @@
             <?php 
                 $topMosaics = [                    
                     ['title' => 'Facciamo un C.V. vincente', 'call' => '', 'cover' => 'curriculum.jpg', 'url' => '/facciamo-cv-vicente', 'cover-type' => 1],
-                    ['title' => 'TV Sogniamo in grande', 'call' => 'Watch more videos', 'cover' => 'youtube.jpg', 'url' => 'https://www.youtube.com/channel/UCL6PL_3UhIO-VqFJ_IJWu3g', 'cover-type' => 1],
+                    //['title' => 'TV Sogniamo in grande', 'call' => 'Watch more videos', 'cover' => 'youtube.jpg', 'url' => 'https://www.youtube.com/channel/UCL6PL_3UhIO-VqFJ_IJWu3g', 'cover-type' => 1],
+                    ['title' => 'Il saturatore', 'call' => '', 'cover' => '9.jpg', 'url' => 'http://www.saturatore.it/', 'cover-type' => 1],
                 ];
 
                 $bottomMosaics = [
-                    ['title' => 'Il saturatore', 'call' => '', 'cover' => '9.jpg', 'url' => 'http://www.saturatore.it/', 'cover-type' => 1],
-                    ['title' => 'Blog and News', 'call' => '', 'cover' => 'blog.jpg', 'url' => '/blog', 'cover-type' => 1],
+                    //['title' => 'Il saturatore', 'call' => '', 'cover' => '9.jpg', 'url' => 'http://www.saturatore.it/', 'cover-type' => 1],
+                    //['title' => 'Blog and News', 'call' => '', 'cover' => 'blog.jpg', 'url' => '/blog', 'cover-type' => 1],
                     //['title' => 'ASSIST TO THE WEBINAR.', 'call' => '', 'cover' => 'webinar.jpg', 'url' => '', 'cover-type' => 1]
                 ];
             ?>
