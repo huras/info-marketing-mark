07098519605f83031e5ff93b7d83c552b78759b0
huras <hurast@gmail.com>
2019-02-19 20:16:24 -0300

    Social Page Background parallax

--- resources/views/pages/social.blade.php
+++ resources/views/pages/social.blade.php
@@ -10,18 +10,24 @@
         ];
     ?>
 
-    <div class='container'>
-        <div class='row' style='margin: 64px 0px;'>
-            <div class='col-12'>
-                <h1> Seguimi sui Canali Social </h1>
-            </div>
-            @foreach($images as $entry)
+        <div class='container'>
+            <div class='row' style='margin: 64px 0px;'>
                 <div class='col-12'>
-                    <a href='{{$entry["url"]}}'> <img style='margin:36px 0px; width: 100%; height: auto;' src='{{asset("img/site/".$entry["image"])}}'> </a>
+                    <h1 style='z-index: 5;'> Seguimi sui Canali Social </h1>
                 </div>
-            @endforeach
+                <div class='paralax n1' style='display: flex; flex-direction: column; justify-content: center; align-items: center; position: relative; height: 1900px; background-image:url("{{asset("img/site/Social BG.png")}}"); background-position: center; background-size: cover; background-position-x: -261px;'>
+                    <div style='width: 100%; width: 100%; height: 100%; background-color: black; opacity: 0.7; position: absolute; left: 0; top: 0;'></div>
+                    <div class='w-100'>
+                        @foreach($images as $entry)
+                            <div class='col-12'>
+                                <a href='{{$entry["url"]}}'> <img style='margin:36px 0px; width: 100%; height: auto;' src='{{asset("img/site/".$entry["image"])}}'> </a>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+    
 
     @include('partials/footer')
 @endsection
