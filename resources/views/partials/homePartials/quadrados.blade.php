525f71bdc73d4f7358c07b8bb912d862b4a41f45
huras <hurast@gmail.com>
2019-02-24 23:31:41 -0300

    Home - Quadrados escondidos

--- resources/views/partials/homePartials/quadrados.blade.php
+++ resources/views/partials/homePartials/quadrados.blade.php
@@ -2,9 +2,9 @@
     <div class='mosaics row'>
             <?php 
                 $topMosaics = [                    
-                    ['title' => 'Facciamo un C.V. vincente', 'call' => '', 'cover' => 'curriculum.jpg', 'url' => '/facciamo-cv-vicente', 'cover-type' => 1],
-                    //['title' => 'TV Sogniamo in grande', 'call' => 'Watch more videos', 'cover' => 'youtube.jpg', 'url' => 'https://www.youtube.com/channel/UCL6PL_3UhIO-VqFJ_IJWu3g', 'cover-type' => 1],
-                    ['title' => 'Il saturatore', 'call' => '', 'cover' => '9.jpg', 'url' => 'http://www.saturatore.it/', 'cover-type' => 1],
+                    //['title' => 'Facciamo un C.V. vincente', 'call' => '', 'cover' => 'curriculum.jpg', 'url' => '/facciamo-cv-vicente', 'cover-type' => 1],
+                    //['title' => 'Il saturatore', 'call' => '', 'cover' => '9.jpg', 'url' => 'http://www.saturatore.it/', 'cover-type' => 1],
+                    //['title' => 'Il saturatore', 'call' => '', 'cover' => '9.jpg', 'url' => 'http://www.saturatore.it/', 'cover-type' => 1],
                 ];
 
                 $bottomMosaics = [
