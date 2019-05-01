3532d3c5a2d8ec44de99c825c8186b8de8912866
huras <hurast@gmail.com>
2019-04-30 23:29:03 -0300

    og ...

--- resources/views/layouts/themed.blade.php
+++ resources/views/layouts/themed.blade.php
@@ -8,10 +8,10 @@
       @yield('meta')
   @else
   @endif -->
-  <meta property="og:image" content="http://www.sogniamoingrande.it/img/site/fb-sharer.png" />
+    <meta property="og:image" content="http://www.sogniamoingrande.it/img/site/fb-sharer.png" />
     <meta property="og:url" content="http://www.sogniamoingrande.it" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="Sogniamo in Grande" />
+    <meta property="og:title" content="Sogniamo in Grandes" />
     <meta property="og:description" content="Maritime training and orientation" />
     <meta property="og:image:type" content="image/png" />
     <meta property="og:image:width" content="200" />
