a775cd166dac150d78f0f10de130b6fa428d56e2
huras <hurast@gmail.com>
2019-07-06 11:24:54 -0300

    About us update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -14,7 +14,48 @@
                 <div class='text-content'> 
                     Sono l’unico italiano di un equipaggio di seicento persone, ma domani potremmo essere molti di più a ripopolare i mari e sollevare in alto il buon nome lasciatoci in eredità dai nostri antenati. Platone diceva che esistono tre tipologie di uomini, i vivi, i morti e quelli che vanno per mare. Oggi mi piacerebbe rilanciare, i vivi, i morti, e quelli che sanno sognare, perché questi ultimi non moriranno mai e tantomeno vivranno come gli altri, ma molto più in alto.
                 </div>
+                <br>
             </div>
+        </div>
+
+        <!-- Claudia Pirozzi -->
+        <div class='col-md-12 cover' style="background-image: url('{{asset('img/site/about/Foto-Claudia-Pirozzi.jpg')}}'); height: 580px;    background-position: center;    background-size: contain;    background-repeat: no-repeat;">
+        </div>
+        <div class='col-md-12 texts'>
+            <div class='text-content'> 
+                Docente e Traduttrice freelance di Inglese tecnico, laureata a pieni voti in Mediazione Linguistica e Culturale inglese/spagnolo presso la Facoltà per Interpreti e Traduttori dell’attuale Università degli Studi Internazionali di Roma, con esperienza decennale nell’insegnamento dell’Inglese marittimo presso i maggiori centri di addestramento e formazione marittima in Napoli e provincia.
+            </div>
+            <br>
+        </div>
+
+        <!-- Claudia Pirozzi -->
+        <div class='col-md-12 cover' style="background-image: url('{{asset('img/site/about/Nino.jpg')}}'); height: 580px;    background-position: center;    background-size: contain;    background-repeat: no-repeat;">
+        </div>
+        <div class='col-md-12 texts'>
+            <div class='text-content'> 
+                Comandante di lungo corso. Diplomato col massimo dei voti presso ITN Nino Bixio di Piano di Sorrento. Impiegato per 18 anni presso la MSC, attualmente impiegato presso la Grimaldi Napoli.
+            </div>
+            <br>
+        </div>
+
+        <!-- Salvatore -->
+        <div class='col-md-12 cover' style="background-image: url('{{asset('img/site/about/salvatore.jpg')}}'); height: 690px;    background-position: 0% 18%;">">
+        </div>
+        <div class='col-md-12 texts'>
+            <div class='text-content'> 
+                Salvatore Smelzo. Diplomato in Ragioneria 96/100. Amministratore e Contabile della gestione clienti del progetto SOGNIAMO IN GRANDE. Impegnato da anni in progetti umanitari senza scopo di lucro. Il suo entusiasmo e la sua competenza nella gestione e organizzazione di nuovi  progetti è stata fondamentale per il progetto SOGNIAMO IN GRANDE.
+            </div>
+            <br>
+        </div>
+
+        <!-- Giorgio -->
+        <div class='col-md-12 cover' style="background-image: url('{{asset('img/site/about/Huras.jpg')}}'); height: 580px;    background-position: center;    background-size: contain;    background-repeat: no-repeat;">
+        </div>
+        <div class='col-md-12 texts'>
+            <div class='text-content'> 
+                Im a web developer and head o the TI department of SOGNIAMO IN GRANDE. I got a degree on computer science in a Brazilian university called UFSJ. I currently study machine learning and web marketing. Currently I also work as a full stack developer from Brasil.
+            </div>
+            <br>
         </div>
     </div>
     @include('partials/footer')
