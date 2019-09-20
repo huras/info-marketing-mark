218a6a0995acd6ea058bb18f6644ab7f0e246873
Huras Alexandre <hurast@gmail.com>
2019-09-19 21:06:13 -0300

    feat: animações nos icones do layout2 index

--- public/js/custom.js
+++ public/js/custom.js
@@ -11,3 +11,47 @@ $('.layout2-home .top-section .caroussel').slick({
   slidesToScroll: 1,
   pauseOnHover: false,
 });
+
+window.addEventListener('scroll', function () {
+  loop();
+});
+
+var elementsToShow = document.querySelectorAll('.animate-on-scroll');
+console.log(elementsToShow);
+
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+function loop() {
+
+  elementsToShow.forEach(function (element) {
+    var animation = element.getAttribute('my-animation');
+
+    if (isElementInViewport(element)) {
+      element.classList.add(animation);
+    } else {
+      //element.classList.remove(animation);
+    }
+  });
+
+  //scroll(loop);
+}
+
+$(document).ready(function () {
+  loop();
+});
